f800a7c19aba5b32a6e8e2c7cd5f4c18ddfaf20c
benchiang5566 <bensonchiang0606@gmail.com>
2026-06-11 15:22:46 +0800

    week16
    week16

---
+++ week16/week16-2.py
@@ -0,0 +1,17 @@
+# week16-2.py 學習計畫 Backtracking 第2題 
+# LeetCode 216. Combination Sum III
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+        # 現在是到哪一個數 ? k 還又幾個數 n : 還要補多少
+        def helper(now, i, k, n): # now : 現在在累積的數
+            if k==0 and n==0: # 成功了!
+                ans.append(now) # 就把 now 塞入 ans 裡
+                return 
+            if k<=0 or n<=0: return # 走完「函式呼叫函式」, 終止
+            for ii in range(i, 10): # i ... 9 之間的數
+                # 現在如果放入 ii
+                helper(now+[ii], ii+1, k-1, n-ii)
+                # 下次要試 ii+1, 用掉1個數, 總和少 ii
+        helper([], 1, k, n)
+        return ans
